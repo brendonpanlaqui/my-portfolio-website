68d9831fa197f53530b71ba9fd21a48d1723c710
Brendon <arbetpanlaqui874@gmail.com>
2025-11-15 21:10:12 +0800

    hero & about

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer id="footer" class="footer">
+    <div class="container">
+      <div class="copyright text-center ">
+        <p>© <span>2025</span> <strong class="px-1 sitename">Brendon Panlaqui.</strong> <span>All rights reserved.<br></span></p>
+      </div>
+      <div class="social-links d-flex justify-content-center">
+        <a href=""><i class="bi bi-twitter-x"></i></a>
+        <a href=""><i class="bi bi-facebook"></i></a>
+        <a href=""><i class="bi bi-instagram"></i></a>
+        <a href=""><i class="bi bi-linkedin"></i></a>
+      </div>
+    </div>
+</footer>
