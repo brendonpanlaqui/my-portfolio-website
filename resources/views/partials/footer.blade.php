44c4f1e6af25c43909aad9a43b7dc8af629d270c
Brendon <arbetpanlaqui874@gmail.com>
2026-01-29 15:50:02 +0800

    edit horasko images

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,6 +3,5 @@
       <div class="copyright text-center ">
         <p>© <span>2025</span> <strong class="px-1 sitename">Brendon Panlaqui.</strong> <span>All rights reserved.<br></span></p>
       </div>
-     
     </div>
 </footer>
